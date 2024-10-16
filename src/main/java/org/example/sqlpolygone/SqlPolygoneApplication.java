43915be5b718be9006150178d65cc62334d293ee
Alibek Birlikbai <87764579+alibekbirlikbai@users.noreply.github.com>
2024-10-16 19:52:37 +0500

    first commit

---
+++ src/main/java/org/example/sqlpolygone/SqlPolygoneApplication.java
@@ -0,0 +1,12 @@
+package org.example.sqlpolygone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlPolygoneApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SqlPolygoneApplication.class, args);
+  }
+}
---
+++ src/test/java/org/example/sqlpolygone/SqlPolygoneApplicationTests.java
@@ -0,0 +1,12 @@
+package org.example.sqlpolygone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlPolygoneApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+}
